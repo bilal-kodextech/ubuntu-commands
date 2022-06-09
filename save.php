b41214efc1d806be82b84aaaf523b4aef2f0578f
Muhammad Bilal <muhammad.bilal@novatoresols.com>
2022-06-09 12:13:46 +0500

    change in jquery

--- save.php
+++ save.php
@@ -1,4 +1,4 @@
-<!-- jquery file upload  -->
+<!-- jquery Validation  -->
 
 <!-- also error message customize colour and placement  -->
 {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.js"></script>
@@ -33,4 +33,4 @@
 });
 </script> --}}
 
-<!-- jquery file upload  -->
+<!-- jquery Validation  -->
